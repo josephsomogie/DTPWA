f3a6c821defde6ead72ff8d49d760ac8e9bca1f4
josephsomogie <128629560+josephsomogie@users.noreply.github.com>
2025-02-12 13:16:36 -0500

    init next site

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # DTPWA
 Decentralized Transfer Protocol Web Application
+
+===================================== Plan
+Create codeless backend for DTP, via webapp
+allow DTPClient to be installable from nodejs, pip, etc.
+allow local storage DTP to be integrated into projects
+======================================
+
+
+flow:
+1. user creates instance of backend framework
+2. user utilizes libs (from node, pip) to initialize a DTPCLient() object, which will init on connection established to backend.
+
+DTPClient() Must:
+- connect to backend
+- send data to backend
+- receive data from backend
+- handle errors
+- handle disconnections
+- process requests from backend
+- process rules
+
+
+Backend: Users can script rules for servers, centralized DB, etc.
+
+
+connection flow:
+1. Server at rest ()
+
+
+
+
+
+
---
+++ app/src/pages/index.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <div
+      className={`flex flex-col w-screen h-screen items-center justify-center bg-gradient-to-r from-neutral-900 via-neutral-950 to-neutral-900`}
+    >
+      <div className="w-full h-16 flex justify-center items-center">
+        <p
+          
+          className="cursor-pointer animate-pulse bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent text-6xl font-mono "
+        >
+          Decentralized Tranfer Protocol
+        </p>
+      </div>
+      <div className="w-1/2 mt-5  flex flex-row items-center justify-evenly font-mono">
+        <p 
+        onClick={() => router.push("/about")}
+        className="hover:outline hover:outline-cyan-200 p-1 rounded cursor-pointer bg-gradient-to-r from-cyan-200 to-blue-400 bg-clip-text text-transparent">
+          About
+        </p>
+        <p className="hover:outline hover:outline-cyan-200 p-1 rounded cursor-pointer bg-gradient-to-r from-cyan-300 to-blue-600 bg-clip-text text-transparent">
+          Installation
+        </p>
+        <p className="hover:outline hover:outline-cyan-200 p-1 rounded cursor-pointer bg-gradient-to-r from-cyan-100 to-blue-300 bg-clip-text text-transparent">
+          Documentation
+        </p>
+        <p className="hover:outline hover:outline-cyan-200 p-1 rounded cursor-pointer bg-gradient-to-r from-cyan-300 to-blue-600 bg-clip-text text-transparent">
+          Online Demo
+        </p>
+      </div>
+    </div>
+  );
+}
